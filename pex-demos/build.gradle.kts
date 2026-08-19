351dfdfec533c85e5d84b46616ef66a93850ef02
Sergey Sidorov <sergey.sidorov@Sergeys-MacBook-Pro.local>
2026-08-19 23:01:55 +0300

    pex-demos: add runAllDemos task to build.gradle.kts

--- pex-demos/build.gradle.kts
+++ pex-demos/build.gradle.kts
@@ -48,3 +48,10 @@ dependencies {
     implementation("org.slf4j:slf4j-api:$slf4jVersion")
     implementation("org.slf4j:slf4j-simple:$slf4jVersion")
 }
+
+tasks.register<JavaExec>("runAllDemos") {
+    description = "Run all PEX demos"
+    group = "verification"
+    mainClass = "ssg.pex.demo.DemoRunner"
+    classpath = sourceSets["main"].runtimeClasspath
+}
